721bc64aae4f2330d25e0d1823fef4c389928bd7
Fernando Costa <afjblt123@gmail.com>
2020-03-26 17:18:33 -0400

    Front-end do app

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api'; 
 
 import logoImg from '../../assets/logo.svg';
 
 import './styles.css';
 
 export default function NewIncidents() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const ongId = localStorage.getItem('ongId')
+
+  const history = useHistory();
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers : {
+          Authorization: ongId,
+        }
+      })
+
+      history.push('/profile');
+    }catch(err) {
+      alert('Erro ao cadastrar caso, tente novamente');
+    }
+  }
+
   return (
     <div className="new-incident">
       <div className="content">
@@ -21,10 +53,10 @@ export default function NewIncidents() {
           Voltar para home
        </Link>
         </section>
-        <form>
-          <input placeholder="Titulo do caso"/>
-          <textarea placeholder="Descrição"/>
-          <input placeholder="Valor em reais"/>
+        <form onSubmit={handleNewIncident}>
+          <input placeholder="Titulo do caso" value={title} onChange={e => setTitle(e.target.value)}/>
+          <textarea placeholder="Descrição" value={description} onChange={e => setDescription(e.target.value)}/>
+          <input placeholder="Valor em reais" value={value} onChange={e => setValue(e.target.value)}/>
           <button className="button">Cadastrar</button>
         </form>
       </div>
